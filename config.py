4e16aa75ef05e18d4d4c5ebacc41bd4528a69606
Le Viet Anh Adrian <lilistrocel@gmail.com>
2025-02-18 19:18:52 +0400

    newconfig

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -6,8 +7,16 @@ from firebase_admin import credentials, firestore
 # Load environment variables
 load_dotenv()
 
+# Check if running on Render
+if os.getenv('RENDER'):
+    # Use JSON string from environment variable
+    cred_dict = json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+    cred = credentials.Certificate(cred_dict)
+else:
+    # Use local file path
+    cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+
 # Initialize Firebase Admin with your service account credentials
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
 firebase_admin.initialize_app(cred)
 
 # Get Firestore client
